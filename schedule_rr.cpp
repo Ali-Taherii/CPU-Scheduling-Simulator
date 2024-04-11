4f5802d2704df9203ab6b7d8584df68e25eccbb3
AndreasY12 <115213306+AndreasY12@users.noreply.github.com>
2024-04-11 20:42:40 +0300

    Modified RR Algo with new implementation

--- schedule_rr.cpp
+++ schedule_rr.cpp
@@ -2,20 +2,21 @@
 
 RR::RR(int quantum) : timeQuantum(quantum) {}
 
+void RR::addTask(const NonPriorityTask& task) {}
 
-void Scheduler::addTask(const Task& task) {
-    taskList.push_front(task);
+void RR::addTask(const PriorityTask& task) {
+    p_taskList.push_front(task);
 }
 
 void RR::scheduleTasks(CPU& cpu) {
-    while (!taskList.empty()) {
+    while (!p_taskList.empty()) {
         //Execute the tasks in a round-robin fashion with a time slice of 10
-        Task currentTask = taskList.front();
+        PriorityTask currentTask = p_taskList.front();
         cpu.runTask(currentTask);
-        taskList.pop_front();
+        p_taskList.pop_front();
         if (currentTask.getRemainingBurst() > timeQuantum) {
             currentTask.setRemainingBurst(currentTask.getRemainingBurst() - timeQuantum);
-            taskList.push_back(currentTask);
+            p_taskList.push_back(currentTask);
         }
     }
 }
--- schedule_rr.h
+++ schedule_rr.h
@@ -7,6 +7,8 @@ private:
     int timeQuantum;
 
 public:
-    RR(int timeQuantum);
+    RR(int quantum); // Constructor to initialize time quantum
+    void addTask(const NonPriorityTask& task) override;
+    void addTask(const PriorityTask& task) override; // Override addTask to maintain sorted order
     void scheduleTasks(CPU& cpu) override;
 };
