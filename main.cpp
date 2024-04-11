4a86b1a5d4890c9464942db7dbb522a7f70811d3
AndreasY12 <115213306+AndreasY12@users.noreply.github.com>
2024-04-11 21:08:09 +0300

    Altered main for testing

--- main.cpp
+++ main.cpp
@@ -1,34 +1,51 @@
-#include <iostream>
-#include "task.h"
-#include "schedule_sjf.h"
+#include "priority_task.h"
+#include "nonpriority_task.h"
 #include "schedule_rr.h"
+#include "schedule_sjf.h"
 #include "cpu.h"
+
+#include <iostream>
 
 int main() {
     // Create tasks
-    Task task1("Task1", 1, 10);
-    Task task2("Task2", 2, 5);
-    Task task3("Task3", 3, 8);
-    Task task4("Task4", 3, 5);
-    Task task5("Task5", 3, 81);
-    Task task6("Task6", 3, 8);
+    NonPriorityTask task1("Task1", 10);
+    NonPriorityTask task2("Task2", 5);
+    NonPriorityTask task3("Task3", 8);
+    NonPriorityTask task4("Task4", 5);
+    NonPriorityTask task5("Task5", 20);
+    NonPriorityTask task6("Task6", 12);
 
     // Create a CPU
     CPU cpu;
 
-    // Create a FCFS scheduler
-    RR rr_scheduler(10);
+    // Create a RR scheduler
+    RR RR_Scheduler(10);
+    // Create a SJF scheduler
+    SJF ShortestJob;
 
-    // Add tasks to the scheduler
-    rr_scheduler.addTask(task1);
-    rr_scheduler.addTask(task2);
-    rr_scheduler.addTask(task3);
-    rr_scheduler.addTask(task4);
-    rr_scheduler.addTask(task5);
-    rr_scheduler.addTask(task6);
+    // Add tasks to the RR Scheduler
+    RR_Scheduler.addTask(task1);
+    RR_Scheduler.addTask(task2);
+    RR_Scheduler.addTask(task3);
+    RR_Scheduler.addTask(task4);
+    RR_Scheduler.addTask(task5);
+    RR_Scheduler.addTask(task6);
 
-    // Schedule and execute tasks
-    rr_scheduler.scheduleTasks(cpu);
+    // Execute tasks
+    std::cout << "RR Algorithm:\n";
+    RR_Scheduler.scheduleTasks(cpu);
+
+    // Add tasks to the SJF Scheduler
+    ShortestJob.addTask(task1);
+    ShortestJob.addTask(task2);
+    ShortestJob.addTask(task3);
+    ShortestJob.addTask(task4);
+    ShortestJob.addTask(task5);
+    ShortestJob.addTask(task6);
+
+    // Execute tasks
+    std::cout << "\nShortestJob Algorithm:\n";
+    ShortestJob.scheduleTasks(cpu);
 
     return 0;
 }
