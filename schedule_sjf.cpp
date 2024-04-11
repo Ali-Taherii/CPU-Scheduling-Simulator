297dd51881bfcbc12cda941f05d672ae648cc0bf
AndreasY12 <115213306+AndreasY12@users.noreply.github.com>
2024-04-11 20:39:36 +0300

    Modified SJF Algo with new implementation

--- schedule_sjf.cpp
+++ schedule_sjf.cpp
@@ -1,29 +1,26 @@
 #include "schedule_sjf.h"
 
 
-void SJF::addTask(const Task& task) {
-    taskList.push_back(task);
+void SJF::addTask(const NonPriorityTask& task) {
+    np_taskList.push_back(task);
 }
 
+void SJF::addTask(const PriorityTask& task) {}
 
 void SJF::scheduleTasks(CPU& cpu) {
-    while (!taskList.empty()) {
+    while (!np_taskList.empty()) {
         //Iterate through the queue to find the shortest job (burst time), then run it and remove it from the queue
 
-        Task shortestJob = taskList.front();
-        for (int i = 0; i < taskList.size(); i++) {
-            if (shortestJob.getBurst() > taskList.front().getBurst()) {
-                shortestJob = taskList.front();
+        NonPriorityTask shortestJob = np_taskList.front();
+        for (int i = 0; i < np_taskList.size(); i++) {
+            if (shortestJob.getBurst() > np_taskList.front().getBurst()) {
+                shortestJob = np_taskList.front();
                 break;
             }
-            taskList.push_back(taskList.front());
-            taskList.pop_front();
+            np_taskList.push_back(np_taskList.front());
+            np_taskList.pop_front();
         }
         cpu.runTask(shortestJob);
-        taskList.pop_front();
-        
-        
-
-        
+        np_taskList.pop_front();
     }
 }
--- schedule_sjf.h
+++ schedule_sjf.h
@@ -4,6 +4,7 @@
 
 class SJF : public Scheduler {
 public:
+    void addTask(const NonPriorityTask& task) override;
+    void addTask(const PriorityTask& task) override;
     void scheduleTasks(CPU& cpu) override;
-    void addTask(const Task& task) override;
 };
